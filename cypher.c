32dbc85fb5fdaa5fb18f1884778d7ba9ecf4dca8
SteevenRodriguez <rodriguez_steven11@hotmail.com>
2017-05-26 20:32:09 -0500

    Falta revisar el codigo morse

--- cypher.c
+++ cypher.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 cypherC(char word[], int key);
-
-int main(int argc, const char* argv[]){
+encodeMorse(char word[]);
+int main(int argc, char** argv){
 if(argc==3){
 	char palabra2[100];
 	strcpy(palabra2,argv[2]);
@@ -48,5 +48,69 @@ for(i = 0; word[i] != '\0'; ++i){
             word[i] = letra;
         }
     }
-printf("Tu clave es %s",word);
+printf("Tu clave es %s ",word);
+encodeMorse(word);
+}
+encodeMorse(char word2[100]){
+int k;
+char** mor;
+for(k = 0; word2[k]!='\0'; ++k){
+	char letra=word2[k];
+	if (letra=='A' || letra=='a')
+		mor[k] = ".-";
+	else if (letra==' ')
+              	mor[k]  = "/";
+	else if (letra=='B' || letra=='b')
+		mor[k] ="-...";
+	else if (letra=='C' || letra=='c')
+		mor[k] = "-.-.";
+	else if (letra=='D' || letra=='d')
+		mor[k] = "-..";
+	else if (letra=='E' || letra=='e')
+		mor[k] = ".";
+	else if (letra=='F' || letra=='f')
+		mor[k] = "..-.";
+	else if (letra=='G' || letra=='g')
+		mor[k] = "--.";
+	else if (letra=='H' || letra=='h')
+		mor[k] = "....";
+	else if (letra=='I' || letra=='i')
+		mor[k] = "..";
+	else if (letra=='J' || letra=='j')
+		mor[k] = ".---";
+	else if (letra=='K' || letra=='k')
+		mor[k] = "-.-";
+	else if (letra=='L' || letra=='l')
+		mor[k] = ".-..";
+	else if (letra=='M' || letra=='m')
+		mor[k] = "--";
+	else if (letra=='N' || letra=='n')
+		mor[k] = "-.";
+	else if (letra=='O' || letra=='o')
+		mor[k] = "---";
+	else if (letra=='P' || letra=='p')
+		mor[k] = ".--.";
+	else if (letra=='Q' || letra=='q')
+		mor[k] = "--.-";
+	else if (letra=='R' || letra=='r')
+		mor[k] = ".-.";
+	else if (letra=='S' || letra=='s')
+		mor[k] = "...";
+	else if (letra=='T' || letra=='t')
+		mor[k] = "-";
+	else if (letra=='U' || letra=='u')
+		mor[k] = "..-";
+	else if (letra=='V' || letra=='v')
+		mor[k] = "...-";
+	else if (letra=='W' || letra=='w')
+		mor[k] = ".--";
+	else if (letra=='X' || letra=='x')
+		mor[k] = "-..-";
+	else if (letra=='Y' || letra=='y')
+		mor[k] = "-.--";
+	else if (letra=='Z' || letra=='z')
+		mor[k] = "--..";
+}
+printf("Su frase en codigo morse es: %s \n",mor);	
+	
 }
